45fb1af0913aa1e2e0c942e211dac0a20adc6e73
Remorc <Remorc@users.noreply.github.com>
2023-07-23 09:11:44 -0500

    Added calculation logic

--- index.js
+++ index.js
@@ -5,21 +5,50 @@ function disableInputs(workOutside, taxRate, income) {
 	income.disabled = true
 }
 
+function calculateCurrentTaxes(workOutside, taxRate, income) {
+	const CURRENT_TAX_RATE = .01
+	const johnstownTaxes = income * CURRENT_TAX_RATE
+	let placeOfWorkTaxes = 0
+	
+	if (workOutside) {
+		placeOfWorkTaxes = income * taxRate	
+	}
+	
+	return johnstownTaxes + placeOfWorkTaxes
+}
+
+function calculateProposedTaxes(workOutside, taxRate, income) {
+	const PROPOSED_TAX_RATE = .0225
+	let placeOfWorkTaxes = 0
+	let taxCredit = 0
+	
+	if (workOutside) {
+		placeOfWorkTaxes = income * taxRate
+		taxCredit = income * Math.min(taxRate, PROPOSED_TAX_RATE) * 0.75
+	}
+
+	const johnstownTaxes = income * PROPOSED_TAX_RATE
+	
+	return johnstownTaxes + placeOfWorkTaxes - taxCredit
+}
+
 function calculateTaxes(workOutside, taxRate, income) {
-	const currentTaxes = Math.trunc(workOutside ? income * taxRate : income * 0.001)
-	const proposedTaxes = Math.trunc(workOutside ? income * taxRate * 0.75 : income * 0.010)
+	const currentTaxes = Math.trunc(calculateCurrentTaxes(workOutside, taxRate, income))
+	const proposedTaxes = Math.trunc(calculateProposedTaxes(workOutside, taxRate, income))
 	const calculatedDifference = Math.trunc(currentTaxes - proposedTaxes)
 	const difference = document.getElementById("difference")
 	
 	document.getElementById("current-taxes-due").innerHTML = `$${currentTaxes}`
 	document.getElementById("proposed-taxes-due").innerHTML = `$${proposedTaxes}`
 	difference.innerHTML = `$${calculatedDifference}`
-	difference.classList.remove(["green", "red"])
+	difference.classList.remove("red")
+	difference.classList.remove("green")
 	
-	if (difference > 0) {
+	console.log(calculatedDifference)
+	if (calculatedDifference > 0) {
 		difference.classList.add("green")
 	}
-	else if (difference < 0) {
+	else if (calculatedDifference < 0) {
 		difference.classList.add("red")
 	}
 }
@@ -36,7 +65,7 @@ function onSubmit() {
 	
 	document.getElementById("outputs").classList.remove("hidden")
 	disableInputs(workOutside, taxRate, income)
-	calculateTaxes(workOutside.value, taxRate.value, income.value)
+	calculateTaxes(workOutside.value === "yes", taxRate.value * .01, income.value)
 
 	return false
 }
